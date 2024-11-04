ca0e56c7e85f32e8cc689945ad7a31d197d63489
Youssef Ghorab <youssef.ghorab@hotmail.com>
2024-11-04 18:09:43 +0200

    Add check to prevent adding users above max users
    add code comments for the entire file

--- Intro_C++_Project/task.cpp
+++ Intro_C++_Project/task.cpp
@@ -1,11 +1,5 @@
 
-/*  
-
-
-
-
-
-*/
+//C++ program that implements a simple user signup application
 
 // Dependencies
 #include <iostream>
@@ -39,6 +33,7 @@ class user_info
 
 // Functions prototypes
 void print_message(string msg);
+void print_message(string msg);
 void print_message();
 record_options read_user_input(void);
 
@@ -57,10 +52,18 @@ class application
     // add user function
     void add_user_record(const string &name,const int &age)
     {
-        users[user_index].username = name;
-        users[user_index].age = age;
-        user_index++;
-        cout << "User record has been added successfully. \n \n";
+        if (user_index < MAX_USERS)
+        {
+            users[user_index].username = name;
+            users[user_index].age = age;
+            user_index++;
+            cout << "User record has been added successfully. \n \n";
+        }
+        else
+        {
+            cout << "Error - User record has reached its limit. \n \n";
+        }
+
     }
 
     // get user function
@@ -73,7 +76,9 @@ class application
         cout << users[id].age;
         print_message("\n\n");
     }
-     
+
+
+    
     // verify if id is greater than number of users added 
     bool is_valid_id(const int &id)
     {
@@ -86,6 +91,7 @@ class application
 
     }
 
+
 };
 
 // --------------------------------------------- //
@@ -95,6 +101,14 @@ class application
 // Functions
 
 
+/**
+ * @brief Main function of the User Signup Application.
+ * 
+ * This function initializes the application, handles user input, and manages
+ * the main loop for adding and fetching user records.
+ * 
+ * @return int Returns 0 upon successful execution.
+ */
 int main()
 {
     bool isRunning = true;
@@ -108,43 +122,57 @@ int main()
 
     while (isRunning == true)
     {
+        // Print the menu options to the console
          print_message();
+         // Read the user's input for the menu option
          options = read_user_input();
          print_message("\n");
 
          switch (options)
          {
          case record_options::ADD:
+            // Ignore any leftover newline characters in the input stream
             cin.ignore();
+
+            // Prompt the user to enter the user's name and age
             print_message("Add user. Please enter user name and age:\n");
             print_message("Name: ");
             getline (cin,name);
             print_message("Age: ");
             cin >>age;
             print_message("\n");
+
+             // Store the user's record to the application
             user_information.add_user_record(name,age);
             break;
 
          case record_options::FETCH:
+            // Prompt the user to enter the user's ID
             print_message("Please enter user ID: \n");
             print_message("User ID: ");
             cin >>id;
+
+            // Check if the entered ID is valid
             if (user_information.is_valid_id(id) == true)
             {
+                 // Fetch and display the user's record
                 user_information.get_user_record(id-1);
             }
             else
             {
+                // Print an error message if the ID is invalid
                 print_message("Invalid User ID \n \n");
             }
 
             break;
 
         case record_options::QUIT:
+            // Set the isRunning flag to false to exit the main loop
              isRunning = false;
             break;
-         
+
          default:
+              // Print an error message if the user enters an invalid option
               print_message("Invalid input please try again\n\n");
             break;
          }
@@ -157,13 +185,26 @@ int main()
     return 0;
 }
 
-
+/**
+ * @brief Prints a message to the standard output.
+ * 
+ * This function outputs the provided string message to the console.
+ * 
+ * @param msg The message to be printed.
+ */
 void print_message(string msg)
 {
     cout <<msg;
 }
 
+
 // Override print message to print the options
+/**
+ * @brief Displays the available options to the user.
+ * 
+ * This function prints the menu options for the user to select from,
+ * including adding a record, fetching a record, or quitting the application.
+ */
 void print_message()
 {   
     cout <<"Please select an option:\n";
@@ -173,10 +214,14 @@ void print_message()
     cout <<"Enter option: ";
 }
 
+
+// Read user input
 record_options read_user_input(void)
 {
     int input = 0;
     cin >>input;
     return static_cast<record_options>(input);
 }
+
+
 
